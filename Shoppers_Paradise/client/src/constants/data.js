4164a3d94d60291bb8ad4be7308357c62218e3bd
santhu6911 <150025727+santhu6911@users.noreply.github.com>
2024-03-25 00:03:26 +0000

    added images

---
+++ Shoppers_Paradise/client/src/constants/data.js
@@ -0,0 +1,293 @@
+export const navData = [
+  {
+    url: "https://rukminim1.flixcart.com/flap/128/128/image/e2b2795ad9e97a57.png?q=100",
+    text: "Mobiles",
+    href: "mobiles",
+  },
+  {
+    url: "https://i.ibb.co/cXFnLLV/3.png",
+    text: "Fashion",
+    href: "fashion",
+  },
+  {
+    url: "https://rukminim1.flixcart.com/flap/128/128/image/9468f7c638007682.png?q=100",
+    text: "Electronics",
+    href: "electronics",
+  },
+  
+  {
+    url: "https://rukminim1.flixcart.com/flap/128/128/image/2a284788d550ac21.png?q=100",
+    text: "Furniture",
+    href: "furniture",
+  },
+  {
+    url: "https://rukminim1.flixcart.com/flap/128/128/image/db4875975dc033aa.png?q=100",
+    text: "Beauty",
+    href: "beauty-toy-products",
+  },
+  {
+    url: "https://rukminim1.flixcart.com/flap/128/128/image/0a4e27792f699b67.png?q=100",
+    text: "Home",
+    href: "home-items",
+  },
+  {
+    url: "https://rukminim1.flixcart.com/flap/128/128/image/71050627a56b4693.png?q=100",
+    text: "Travel",
+    href: "travel",
+  },
+  {
+    url: "https://rukminim1.flixcart.com/flap/128/128/image/d42b57d8569fccc3.png?q=100",
+    text: "Appliances",
+    href: "appliances",
+  },
+  {
+    url: "https://rukminim1.flixcart.com/flap/128/128/image/d270faabceb32f09.png?q=100",
+    text: "Grocery",
+    href: "grocery",
+  },
+];
+
+// ########## Posters ##########################
+
+export const bannerData = [
+ 
+    "https://m.media-amazon.com/images/I/61hIbVWqDiL._SX3000_.jpg",
+    "https://m.media-amazon.com/images/I/71i-6k8LoBL._SX3000_.jpg",
+    "https://m.media-amazon.com/images/I/619xcE1JVEL._SX3000_.jpg",
+    "https://m.media-amazon.com/images/I/71krlR65lnL._SX3000_.jpg"
+  
+];
+
+export const posterLinks = {
+  links1: [
+    "https://m.media-amazon.com/images/I/619xcE1JVEL._SX3000_.jpg",
+    "https://m.media-amazon.com/images/I/71krlR65lnL._SX3000_.jpg",
+  ],
+
+  links2: [
+    "https://rukminim1.flixcart.com/flap/1000/1000/image/bf9c7eb4dfe3e778.jpg?q=50",
+    "https://rukminim1.flixcart.com/flap/1000/1000/image/9123cc58a752f8f3.jpg?q=50",
+    "https://rukminim1.flixcart.com/flap/1000/1000/image/89d5eee1f8f1e530.jpeg?q=50",
+  ],
+
+  links3: [
+    "https://m.media-amazon.com/images/I/7102I1D+77L._SX3000_.jpg",
+    "https://m.media-amazon.com/images/I/7102I1D+77L._SX3000_.jpg",
+    "https://m.media-amazon.com/images/I/7102I1D+77L._SX3000_.jpg",
+  ],
+
+  links4: [
+    "https://rukminim1.flixcart.com/flap/1000/1000/image/f40f2aeeeaffb5c8.jpeg?q=50",
+    "https://rukminim1.flixcart.com/flap/1000/1000/image/8ad9432b231a6ff2.jpeg?q=50",
+    "https://rukminim1.flixcart.com/flap/1000/1000/image/e44a19df21425913.jpeg?q=50",
+  ],
+
+  links5: [
+    "https://rukminim1.flixcart.com/flap/1000/1000/image/2a7f67fc8476bec7.jpeg?q=50",
+    "https://rukminim1.flixcart.com/flap/1000/1000/image/02f31191fe477de3.jpg?q=50",
+    "https://rukminim1.flixcart.com/flap/1000/1000/image/5d7822bf05bdce34.jpg?q=50",
+  ],
+
+  links6: [
+
+    "https://m.media-amazon.com/images/I/7102I1D+77L._SX3000_.jpg",
+    "https://m.media-amazon.com/images/I/61hIbVWqDiL._SX3000_.jpg",
+    "https://m.media-amazon.com/images/I/7102I1D+77L._SX3000_.jpg",
+  ],
+};
+
+// #### Featured Brand Links #######
+
+export const featuredBrandLinks = {
+  links1: [
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Egypt.svg/2560px-Flag_of_Egypt.svg.png",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Egypt.svg/2560px-Flag_of_Egypt.svg.png",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Egypt.svg/2560px-Flag_of_Egypt.svg.png",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Egypt.svg/2560px-Flag_of_Egypt.svg.png",
+
+  ],
+  links2: [
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Egypt.svg/2560px-Flag_of_Egypt.svg.png",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Egypt.svg/2560px-Flag_of_Egypt.svg.png",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Egypt.svg/2560px-Flag_of_Egypt.svg.png",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Egypt.svg/2560px-Flag_of_Egypt.svg.png",
+
+  ],
+};
+
+export const horizontalPosterLink =
+  "https://images-eu.ssl-images-amazon.com/images/G/42/Sunrise/Events/2022/PD22/GW/XCM_Manual_1443555_4810454_400x39_2X._CB632499316_.jpg";
+
+export const sidePosterLink =
+
+  "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
+
+export const maleAvatarUrl =
+  // "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/profile-pic-male_4811a1.svg";
+  "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+
+export const femaleAvatarUrl =
+  // "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/profile-pic-female_0627fd.svg";
+  "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+
+// export const fassured =
+//   "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/fa_62673a.png";
+
+export const emptyWishlist =
+  "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/mywishlist-empty_39f7a5.png";
+
+export const emptyCartUrl =
+  "https://rukminim1.flixcart.com/www/800/800/promos/16/05/2019/d438a32e-765a-4d8b-b4a6-520b560971e8.png?q=90";
+
+export const noOrdersUrl =
+  "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/emptyOrders_f13d28.png";
+
+export const superCoin =
+  "https://rukminim1.flixcart.com/lockin/736/176/images/promotion_banner_v2_inactive_2.png?q=50";
+
+export const shieldIcon =
+  "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/shield_5f9216.png";
+
+export const indianStates = [
+  {
+    value: "Andhra Pradesh",
+    label: "Andhra Pradesh",
+  },
+  {
+    value: "Assam",
+    label: "Assam",
+  },
+  {
+    value: "Bihar",
+    label: "Bihar",
+  },
+
+  {
+    value: "Chhattisgarh",
+    label: "Chhattisgarh",
+  },
+  {
+    value: "Goa",
+    label: "Goa",
+  },
+  {
+    value: "Gujarat",
+    label: "Gujarat",
+  },
+  {
+    value: "Haryana",
+    label: "Haryana",
+  },
+  {
+    value: "Himachal Pradesh",
+    label: "Himachal Pradesh",
+  },
+  {
+    value: "Jammu and Kashmir",
+    label: "Jammu and Kashmir",
+  },
+  {
+    value: "Jharkhand",
+    label: "Jharkhand",
+  },
+  {
+    value: "Karnataka",
+    label: "Karnataka",
+  },
+  {
+    value: "Kerala",
+    label: "Kerala",
+  },
+  {
+    value: "Madhya Pradesh",
+    label: "Madhya Pradesh",
+  },
+  {
+    value: "Maharashtra",
+    label: "Maharashtra",
+  },
+  {
+    value: "Manipur",
+    label: "Manipur",
+  },
+  {
+    value: "Meghalaya",
+    label: "Meghalaya",
+  },
+  {
+    value: "Mizoram",
+    label: "Mizoram",
+  },
+  {
+    value: "Nagaland",
+    label: "Nagaland",
+  },
+  {
+    value: "Odisha",
+    label: "Odisha",
+  },
+  {
+    value: "Punjab",
+    label: "Punjab",
+  },
+  {
+    value: "Rajasthan",
+    label: "Rajasthan",
+  },
+  {
+    value: "Sikkim",
+    label: "Sikkim",
+  },
+  {
+    value: "Tamil Nadu",
+    label: "Tamil Nadu",
+  },
+  {
+    value: "Telangana",
+    label: "Telangana",
+  },
+  {
+    value: "Tripura",
+    label: "Tripura",
+  },
+  {
+    value: "Uttarakhand",
+    label: "Uttarakhand",
+  },
+  {
+    value: "Uttar Pradesh",
+    label: "Uttar Pradesh",
+  },
+  {
+    value: "West Bengal",
+    label: "West Bengal",
+  },
+  {
+    value: "Andaman and Nicobar Islands",
+    label: "Andaman and Nicobar Islands",
+  },
+  {
+    value: "Chandigarh",
+    label: "Chandigarh",
+  },
+  {
+    value: "Dadra and Nagar Haveli",
+    label: "Dadra and Nagar Haveli",
+  },
+  {
+    value: "Daman and Diu",
+    label: "Daman and Diu",
+  },
+  {
+    value: "Delhi",
+    label: "Delhi",
+  },
+  {
+    value: "Lakshadweep",
+    label: "Lakshadweep",
+  },
+  {
+    value: "Puducherry",
+    label: "Puducherry",
+  },
+];
